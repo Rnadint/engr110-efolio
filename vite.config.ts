7fdb2d31440e3888fa59ea122bb73e76247665e6
Rnadint <rnadint@gmail.com>
2026-03-03 11:25:00 -0800

    deploy update

--- vite.config.ts
+++ vite.config.ts
@@ -153,6 +153,7 @@ function vitePluginManusDebugCollector(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector()];
 
 export default defineConfig({
+  base: "/engr110-efolio/",
   plugins,
   resolve: {
     alias: {
